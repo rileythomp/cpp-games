6919424f5a0e9c719ee8da52b50a97038e5d2191
rileythompson <riley@lendingloop.ca>
2019-01-14 09:40:29 -0500

    add color

--- gameoflife/main.cc
+++ gameoflife/main.cc
@@ -4,6 +4,23 @@
 using namespace std;
 
 #define For(i, n) for(int i = 0; i < (n); ++i)
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 void printStart() {
 	For(i, 42) {
@@ -22,12 +39,17 @@ void printStart() {
 	cout << '|' << endl << '|';
 	For(i, 41) {cout << '_';}
 	cout << '|' << endl << endl;
+	cout << RED << "hello" << RESET << endl;
 }
 
 void print(vector<vector<int>> grid) {
     for (auto row : grid) {
         for (auto cell : row) {
-            cout << cell << ' ';
+            if (cell) {
+		cout << RED << "1 " << RESET;
+	    } else {
+	    	cout << cell << ' ';
+	    }
         }
         cout << endl;
     }
